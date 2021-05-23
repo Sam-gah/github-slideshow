cc53800d1fae56c2747ffd74d1a6214aee721e3a
Sam-gah <84658488+Sam-gah@users.noreply.github.com>
2021-05-23 13:29:37 +0545

    Add files via upload

---
+++ armstrong using loop.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n,count=0,a;
+	printf("Enter a number ");
+	scanf("%d",&n);
+	a=n
+	b=a
+	while (n!=0)
+	{
+		count++;
+		num=num/10;
+	}
+		
+		while (a |=0 )
+		{
+			rem=a%10 ;
+			sum += pow(rem,n);
+			a/=10;
+		}
+			
+	if (sum== b )	
+		printf ("ARMstrong ");
+		else
+		printf ("NOt armstrong\\n");
+		
+	}
